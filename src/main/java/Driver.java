8f4715b0560564961dab57f97328d5adc087e23e
nadiaputrirah <nadiapputriirah@gmail.com>
2025-05-29 15:35:09 +0700

    update file driver

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -2,20 +2,18 @@ import src.main.java.Counter;
 
 public class Driver {
 
-	public static void main(String[] args) {
-		
-		Counter counter = new Counter();
-		
-		System.out.println("Current Count:" + counter.getCount());
-		
-		counters.increment();
-		
-		System.out.println("Current Count:" + counter.getCount());
-		
-		counter.decrement();
-		
-		System.out.println("Current Count:" + counter.getCount());
-		
-	}
+    public static void main(String[] args) {
 
+        Counter counter = new Counter();
+
+        System.out.println("Current Count: " + counter.getCount());
+
+        counter.increment();
+
+        System.out.println("Current Count: " + counter.getCount());
+
+        counter.decrement();
+
+        System.out.println("Current Count: " + counter.getCount());
+    }
 }
